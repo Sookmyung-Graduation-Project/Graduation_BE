d20ea9165903da8959b2f24d3a79df54ee73818b
woojin-choi <dnwls0723@sookmyung.ac.kr>
2025-07-18 20:31:31 +0900

    fix: v1 참조 경로 수정

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-# app/main.py
 from fastapi import FastAPI
 from app.api.v1.endpoints.auth import kakao_auth
 from app.routers import system
+from app.routers.v1_router import v1_router
 from app.db import init_db
 import uvicorn
 
@@ -13,6 +13,7 @@ async def start_db():
 
 app.include_router(kakao_auth.router, prefix="/login", tags=["auth"])
 app.include_router(system.router, tags=["system"])
+app.include_router(v1_router, prefix="/api/v1")
 
 @app.get("/")
 async def root():
