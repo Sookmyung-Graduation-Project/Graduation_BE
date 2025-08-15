33d45378259161abf97a436d59ffaa3b6a149000
woojin-choi <dnwls0723@sookmyung.ac.kr>
2025-08-15 19:46:34 +0900

    feat: voice model에 voice_id, description 필드 추가

--- app/models/voice.py
+++ app/models/voice.py
@@ -6,7 +6,9 @@ from typing import Optional
 
 class Voice(Document):
     voice_name: str = "기본 음성"
+    voice_id: str
     user_id: PydanticObjectId
+    description: Optional[str] = None
     created_at: datetime = datetime.now()
 
     class Settings:
